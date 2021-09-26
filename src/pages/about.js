a003d7c8aaf3a42821d0d84eeac693eaa6b9f389
Bart <bartlomiej.mondrzycki@yumasoft.pl>
2021-09-27 00:17:46 +0200

    fixed build errors

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,30 +1,36 @@
 import * as React from "react"
-//import LayoutSubPage from "../js/layouts/layout-sub-page"
 import SEO from "../components/seo"
 import Layout from '../components/layout'
-// import '../style/from-scratch.scss'
+import AboutPhoto from "../yuma_website_assets/ASSETS/aboutusDesktopImage.png"
+import { useIntl } from "react-intl"
+import "../../i18n/react-intl/en.json"
+import "../../i18n/react-intl/pl.json"
 
 const About = () => {
+  const intl = useIntl()
   return (
     <Layout>
       <SEO title="About us" />
-      <h1>ABOUT US</h1>
-      <h2>YUMASOFT - SOFTWARE FOR BUSINESS </h2>
-<p>Our company was established in 1998. Over the years, Yumasoft has become a team where the expertise of experienced specialists blends with the enthusiasm and creativity of young university graduates. It's a perfect mix - proven by years of our prompt, effective and reliable IT services in Wrocław.</p>
+      <div className="container">
+        {/* <h1>{intl(aboutTitle)}</h1> */}
+        <h2>YUMASOFT - SOFTWARE FOR BUSINESS </h2>
+        <p>Our company was established in 1998. Over the years, Yumasoft has become a team where the expertise of experienced specialists blends with the enthusiasm and creativity of young university graduates. It's a perfect mix - proven by years of our prompt, effective and reliable IT services in Wrocław.</p>
 
 
-<h2><em>OVER 30 CUSTOMERS, NEARLY 100 PROJECTS</em></h2>
-<p>More than 30 customers have benefited so far from Yumasoft IT services and outsourcing, with whom we have accomplished over 100 projects. We work with customers in Western Europe and the USA. These are predominantly long-term processes in which a period of dynamic creation is followed by a period of sustainable development. Customers appreciate the flexibility of our team: we use a variety of project implementation methods and we can start the implementation process at any stage of a project. Attention to detail, stability of operation and innovative ideas – these are the steps we take to earn your satisfaction.
+        <h2><em>OVER 30 CUSTOMERS, NEARLY 100 PROJECTS</em></h2>
+        <p>More than 30 customers have benefited so far from Yumasoft IT services and outsourcing, with whom we have accomplished over 100 projects. We work with customers in Western Europe and the USA. These are predominantly long-term processes in which a period of dynamic creation is followed by a period of sustainable development. Customers appreciate the flexibility of our team: we use a variety of project implementation methods and we can start the implementation process at any stage of a project. Attention to detail, stability of operation and innovative ideas – these are the steps we take to earn your satisfaction.
 
-We are a Microsoft partner. Our developers are MCPD (Microsoft Certified Professional Developer) and MCTS (Microsoft Certified Technology Specialist) certified.
+        We are a Microsoft partner. Our developers are MCPD (Microsoft Certified Professional Developer) and MCTS (Microsoft Certified Technology Specialist) certified.
 
-Reliability of Yumasoft testers is confirmed by ISTQB certifications (International Software Testing Qualifications Board).
+        Reliability of Yumasoft testers is confirmed by ISTQB certifications (International Software Testing Qualifications Board).
 
 
-Efficient business is our priority. We invite you to cooperate with us!
-Yumasoft, IT services, Wrocław
+        Efficient business is our priority. We invite you to cooperate with us!
+        Yumasoft, IT services, Wrocław
 
-</p>
+        </p>
+      </div>
+      <img className="subpage-img" src={AboutPhoto} alt="about us image"></img>
     </Layout>
   )
 }
