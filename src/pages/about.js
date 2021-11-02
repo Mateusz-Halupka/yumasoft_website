11dd50a53ede08f1627e7b90a7871615bd5241a7
Bart <bartlomiej.mondrzycki@yumasoft.pl>
2021-11-02 09:31:43 +0100

    translation demo

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,18 +7,19 @@ import "../../i18n/react-intl/en.json"
 import "../../i18n/react-intl/pl.json"
 
 const About = () => {
-  const intl = useIntl()
+  const intl = useIntl().messages
+
   return (
     <Layout>
       <SEO title="About us" />
       <div className="container">
-        {/* <h1>{intl(aboutTitle)}</h1> */}
-        <h2>YUMASOFT - SOFTWARE FOR BUSINESS </h2>
-        <p>Our company was established in 1998. Over the years, Yumasoft has become a team where the expertise of experienced specialists blends with the enthusiasm and creativity of young university graduates. It's a perfect mix - proven by years of our prompt, effective and reliable IT services in Wrocław.</p>
+        <h1>{intl.aboutTitle}</h1>
+        <h2>{intl.aboutSub1}</h2>
+        <p>{intl.aboutPar1}</p>
+        <h2><em>{intl.aboutSub2}</em></h2>
 
-
-        <h2><em>OVER 30 CUSTOMERS, NEARLY 100 PROJECTS</em></h2>
-        <p>More than 30 customers have benefited so far from Yumasoft IT services and outsourcing, with whom we have accomplished over 100 projects. We work with customers in Western Europe and the USA. These are predominantly long-term processes in which a period of dynamic creation is followed by a period of sustainable development. Customers appreciate the flexibility of our team: we use a variety of project implementation methods and we can start the implementation process at any stage of a project. Attention to detail, stability of operation and innovative ideas – these are the steps we take to earn your satisfaction.
+        {/* TODO: move text below to translation files */}
+        {/* <p>More than 30 customers have benefited so far from Yumasoft IT services and outsourcing, with whom we have accomplished over 100 projects. We work with customers in Western Europe and the USA. These are predominantly long-term processes in which a period of dynamic creation is followed by a period of sustainable development. Customers appreciate the flexibility of our team: we use a variety of project implementation methods and we can start the implementation process at any stage of a project. Attention to detail, stability of operation and innovative ideas – these are the steps we take to earn your satisfaction.
 
         We are a Microsoft partner. Our developers are MCPD (Microsoft Certified Professional Developer) and MCTS (Microsoft Certified Technology Specialist) certified.
 
@@ -28,7 +29,7 @@ const About = () => {
         Efficient business is our priority. We invite you to cooperate with us!
         Yumasoft, IT services, Wrocław
 
-        </p>
+        </p> */}
       </div>
       <img className="subpage-img" src={AboutPhoto} alt="about us image"></img>
     </Layout>
